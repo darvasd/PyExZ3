03b1b62a7a010b4eea102df2aac9decbe599d971
Darvas Dániel <darvas.daniel@gmail.com>
2014-08-17 13:22:15 +0200

    Powtest comment.

--- bad_tests/powtest.py
+++ bad_tests/powtest.py
@@ -3,6 +3,11 @@ def powtest(in1):
         return 1
     elif in1*in1 > 0:
         return 2
+		
+	# This should be impossible, as the square of a real number cannot be negative.
+	# However, because in this implementation Z3 works with bitvectors, this is possible.
+	# PyExZ3 will check if the model given by Z3 is real (executable), but it will cause a looong execution.
+	# If we would use Int, an unsat could be given for And(Not(in1*in1 == 0),Not(in1*in1 > 0)).
     return 0
 
 def expected_result():
